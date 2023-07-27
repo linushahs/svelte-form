ab163fed3c9c6cc0c1577810262d55c14748f339
linushahs <suniltraveler2004@gmail.com>
2023-07-27 22:57:33 +0545

    Added some extra libraries for simplicity.

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,16 +1,10 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/svelte";
+import { expect, test } from "vitest";
 import App from "./App.svelte";
-import { test, expect, afterEach } from "vitest";
-
-let host: HTMLElement;
-
-afterEach(() => {
-  host.remove();
-});
 
 test("App renders successfully", () => {
-  host = document.createElement("div");
-  host.setAttribute("id", "host");
-  document.body.appendChild(host);
-  const instance = new App({ target: host });
-  expect(host.innerHTML).toContain("Hello world");
+  render(App);
+  const heading = screen.getByText("Hello world");
+  expect(heading).toBeInTheDocument();
 });
